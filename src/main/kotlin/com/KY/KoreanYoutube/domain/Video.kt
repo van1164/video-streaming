1288905b750e27d170545a4d70e7ae6989a1a582
van1164 <vkn123@naver.com>
2024-03-13 15:21:36 +0900

    feat: 🙄동영상에 대한 정보 저장

--- src/main/kotlin/com/KY/KoreanYoutube/domain/Video.kt
+++ src/main/kotlin/com/KY/KoreanYoutube/domain/Video.kt
@@ -1,3 +1,22 @@
 package com.KY.KoreanYoutube.domain
 
-data class Video()
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "video")
+data class Video(
+
+    @Column(name = "title")
+    var title : String,
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    val createDate : Date,
+
+    @Id
+    var id : String,
+
+    @Column(name = "thumbnail_url")
+    var thumbNailUrl : String,
+)

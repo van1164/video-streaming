9e3a6fc39dd192800059c47ea19a03c84ba9cc34
van1164 <vkn123@naver.com>
2024-04-09 00:23:11 +0900

    feat: domain 생성

---
+++ src/main/kotlin/com/KY/KoreanYoutube/domain/User.kt
@@ -0,0 +1,22 @@
+package com.KY.KoreanYoutube.domain
+
+import jakarta.persistence.*
+import java.util.*
+
+
+@Entity
+@Table(name = "user")
+data class User(
+
+    @Column(name = "name")
+    val name: String,
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    val createDate : Date,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    val id: Long? = null
+)
--- src/main/kotlin/com/KY/KoreanYoutube/domain/Video.kt
+++ src/main/kotlin/com/KY/KoreanYoutube/domain/Video.kt
@@ -10,13 +10,27 @@ data class Video(
     @Column(name = "title")
     var title : String,
 
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false)
     val createDate : Date,
 
     @Id
+    @Column(name = "video_id")
     var id : String,
 
     @Column(name = "thumbnail_url")
     var thumbNailUrl : String?,
+
+
+    @Column(name = "comments")
+    @OneToMany(mappedBy = "id", fetch = FetchType.LAZY)
+    val commentList : MutableList<Comment> = mutableListOf(),
+
+
+    @Column
+    var good : Int = 0,
+
+    @Column
+    var bad : Int = 0,
 )

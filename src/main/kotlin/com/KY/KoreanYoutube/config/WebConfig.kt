4716b4d16f6037dc1c479ba3f7cdb41ad1afd3c0
van1164 <vkn123@naver.com>
2024-04-12 13:41:28 +0900

    chore : 😋 cors 허용 처리 나중에 수정 예정

--- src/main/kotlin/com/KY/KoreanYoutube/config/WebConfig.kt
+++ src/main/kotlin/com/KY/KoreanYoutube/config/WebConfig.kt
@@ -1,4 +1,18 @@
 package com.KY.KoreanYoutube.config
 
-class WebConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600)
+    }
 }
---
+++ src/main/kotlin/com/KY/KoreanYoutube/user/UserController.kt
@@ -0,0 +1,4 @@
+package com.KY.KoreanYoutube.user
+
+class UserController {
+}

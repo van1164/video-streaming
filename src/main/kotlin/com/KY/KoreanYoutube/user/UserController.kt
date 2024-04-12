f09677a4ee77e7b41ef72b6a358d9549775e23bc
van1164 <vkn123@naver.com>
2024-04-12 13:42:14 +0900

    feat: 테스트용 사용자 추가

--- src/main/kotlin/com/KY/KoreanYoutube/user/UserController.kt
+++ src/main/kotlin/com/KY/KoreanYoutube/user/UserController.kt
@@ -1,4 +1,21 @@
 package com.KY.KoreanYoutube.user
 
-class UserController {
+import com.KY.KoreanYoutube.domain.User
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
+
+@Controller
+@RequestMapping("/api/v1/user")
+class UserController(
+    val userRepository: userRepository
+) {
+
+    @GetMapping("/test_create")
+    fun testCreate(){
+        userRepository.save(User("test",  Date.from(Date().toInstant())))
+    }
 }

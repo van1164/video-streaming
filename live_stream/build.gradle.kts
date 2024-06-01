780996025957bd3866e3ba91c250e0967d9ba78c
van1164 <vkn123@naver.com>
2024-06-01 18:09:37 +0900

    chore: 멀티모듈 build gradle 추가

---
+++ live_stream/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.van1164"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}

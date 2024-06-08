623264e9a0c93e3a0c78dda42099a62c4ce8936e
van1164 <vkn123@naver.com>
2024-06-08 22:24:52 +0900

    fix: 컴포넌트 스캔 메인 추가

--- video/src/main/kotlin/com/van1164/video/VideoApplication.kt
+++ video/src/main/kotlin/com/van1164/video/VideoApplication.kt
@@ -8,7 +8,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
-@ComponentScan(basePackages = ["com.van1164.common","com.van1164.util","com.van1164.security","com.van1164.video","com.van1164.main","com.van1164.comment"])
+@ComponentScan(basePackages = ["com.van1164.common","com.van1164.util","com.van1164.security","com.van1164.video","com.van1164.main_module","com.van1164.comment"])
 @EnableR2dbcRepositories(basePackageClasses = [VideoR2DBCRepository::class,VideoLikeRepository::class,VideoViewRepository::class])
 @SpringBootApplication
 class VideoApplication
